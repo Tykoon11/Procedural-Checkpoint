de941e776e6201eefa9b112c16e1ad2e35523bd8
Tykoon11 <97805439+Tykoon11@users.noreply.github.com>
2022-03-10 15:10:55 +0100

    Update procedure.js

--- procedure.js
+++ procedure.js
@@ -5,7 +5,7 @@ function dotProduct(v1, v2){
     for(let i=0; i<v1.length; i++){
     let ps = (v1[i] * v2[i]) + (v1[i+1] * v2[i+1])
     return ps;
-``    }
+   }
 }
 // console.log (dotProduct([3,4],[2,3]))
 
